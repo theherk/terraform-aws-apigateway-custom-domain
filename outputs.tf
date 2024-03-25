3388a0e980c55b3e830a2736ebda72bcc01e3c07
Adam Sherwood <theherk@gmail.com>
2024-03-25 17:07:10 +0100

    🐛 Return null when no service is generated.

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "vpc_endpoint_service" {
   description = "VPC Endpoint Service toward load balancer. This can be used to generate a VPC Endpoint Interface in another connectivity account."
-  value       = try(aws_vpc_endpoint_service.this[0])
+  value       = try(aws_vpc_endpoint_service.this[0], null)
 }
